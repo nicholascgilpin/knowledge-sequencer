8119fe5f547adf0c5d0011af0cd656de972db981
Nick <nicholascgilpin@yahoo.com>
2016-12-19 19:57:25 -0600

    Outlined site framework
    No styling present currently
    Drafted a design for the homepage
    graph.js contains probable graph API
    No user interface/display code yet

---
+++ js/graph.js
@@ -0,0 +1,27 @@
+// Functions pertaining to the knowledge graph
+
+// create a node with a unique id (needed in case of duplicate titles)
+function Node(title) {
+
+}
+
+// designate a node as the prerequisite knowledge for this skill node
+function prerequisite(prerequisite, current) {
+
+}
+
+// create a graph
+function Graph() {
+
+}
+
+// returns the node referenced by the reference
+function find_node(references) {
+
+}
+
+// Find the shortest path from node with start title to node with finish title
+// returns an ordered STACK of node references
+function shortest_path(start,finish) {
+  
+}
---
+++ js/site.js
@@ -0,0 +1,21 @@
+// Description: Site wide functions
+
+// Display Code
+// Determines which node data gets presented where
+current_node = NULL;// Reference to the current node displayed from nav_stack
+nav_stack = NULL;   // Stack/List of skills to learn
+// Replaces text inside specific tags with the new node's content
+function display_node(node) {
+
+}
+// User Interface Buttons
+// Links buttons to graph and display functions
+// Load the next node in the learning path
+function next(nav_stack) {
+
+}
+
+// Load the previous node in the learning path
+function back(nav_stack) {
+
+}
