ff85bcc5c93955c268b94b04deb7505662b3b0ef
Nick <nicholascgilpin@yahoo.com>
2016-12-20 11:13:31 -0600

    Defined nodes and test cases
    Nodes consist of a title, content, and an ID.
    Test cases are a group of functions which verify the output of other
    functions. I used these to verify that the node function works as
    expected.

--- js/graph.js
+++ js/graph.js
@@ -1,8 +1,14 @@
 // Functions pertaining to the knowledge graph
-
+node_ids = -1; // Tracks the last used node ids, so that all ids can be unique
 // create a node with a unique id (needed in case of duplicate titles)
-function Node(title) {
-
+function Node(title = "",content = "", prerequisites = []) {
+  var node = {};
+  node.title = title;
+  node.content = content;
+  node.prerequisites = prerequisites;
+  node_ids = node_ids + 1;
+  node.id = node_ids;
+  return node;
 }
 
 // designate a node as the prerequisite knowledge for this skill node
@@ -23,5 +29,22 @@ function find_node(references) {
 // Find the shortest path from node with start title to node with finish title
 // returns an ordered STACK of node references
 function shortest_path(start,finish) {
-  
 }
+
+
+// Validates functions by checking if the actual output matches the expected output
+function test(test_id, function_output, expected_output){
+  if (function_output == expected_output){
+    console.log("Test " + test_id + " passed!");
+  }
+  else{
+    console.log("Test " + test_id + " failed!");
+    console.log(function_output);
+  }
+}
+
+// Test cases (To be run within this file only)
+test(0,Node("a","a").title,"a");
+test(1,Node("a","b").content,"b");
+test(2,Node("a","c").id,2);
+test(3,Node("a","d",[1]).prerequisites[0],1);
--- js/site.js
+++ js/site.js
@@ -2,8 +2,8 @@
 
 // Display Code
 // Determines which node data gets presented where
-current_node = NULL;// Reference to the current node displayed from nav_stack
-nav_stack = NULL;   // Stack/List of skills to learn
+current_node = null;// Reference to the current node displayed from nav_stack
+nav_stack = [];   // Stack/List of skills to learn
 // Replaces text inside specific tags with the new node's content
 function display_node(node) {
 
@@ -19,3 +19,16 @@ function next(nav_stack) {
 function back(nav_stack) {
 
 }
+
+// Validates functions by checking if the actual output matches the expected output
+function test(test_id, function_output, expected_output){
+  if (function_output == expected_output){
+    console.log("Test " + test_id + " passed!");
+  }
+  else{
+    console.log("Test " + test_id + " failed!");
+    console.log(function_output);
+  }
+}
+
+// Test cases (To be run within this file only)
